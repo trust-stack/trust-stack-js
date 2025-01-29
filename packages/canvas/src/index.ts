c29aafde706fe7ec93fb9825d3c6414b883e3e6c
Harley Thomas <harleyjackthomas@gmail.com>
2025-01-29 17:26:05 +1030

    Adding ability to link canvas-template instances via hermes link resolver.

--- packages/canvas/src/index.ts
+++ packages/canvas/src/index.ts
@@ -1,5 +1,10 @@
 import {Mixin} from "ts-mixer";
 import {CanvasTemplateClient} from "./canvas-template";
+import {CanvasTemplateInstanceClient} from "./canvas-template-instance";
 import {ThemeClient} from "./theme";
 
-export class CanvasClient extends Mixin(CanvasTemplateClient, ThemeClient) {}
+export class CanvasClient extends Mixin(
+  CanvasTemplateClient,
+  CanvasTemplateInstanceClient,
+  ThemeClient
+) {}
