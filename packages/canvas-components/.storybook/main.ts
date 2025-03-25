4cc3d20542da0fc46a96f0fa5f94cb5c32907d63
Harley Thomas <harleyjackthomas@gmail.com>
2025-03-25 16:33:53 +0700

    Updating deps.

--- packages/canvas-components/.storybook/main.ts
+++ packages/canvas-components/.storybook/main.ts
@@ -9,6 +9,7 @@ import { dirname, join } from 'path';
 function getAbsolutePath(value: string): any {
   return dirname(require.resolve(join(value, 'package.json')));
 }
+
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [getAbsolutePath('@storybook/addon-essentials'), { name: 'storybook-addon-stencil', options: { stencilOptions: {} } }],
