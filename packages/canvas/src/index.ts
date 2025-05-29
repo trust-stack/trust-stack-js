e10d9d150622e6dc46ee699708b385c9138fd6ed
Harley Thomas <harleyjackthomas@gmail.com>
2025-05-29 11:22:55 +0930

    Updating openapi schema and frame, theme methods.

--- packages/canvas/src/index.ts
+++ packages/canvas/src/index.ts
@@ -1,5 +1,4 @@
 import {Mixin} from "ts-mixer";
 import {CanvasTemplateClient} from "./canvas-template";
-import {ThemeClient} from "./theme";
 
-export class CanvasClient extends Mixin(CanvasTemplateClient, ThemeClient) {}
+export class CanvasClient extends Mixin(CanvasTemplateClient) {}
